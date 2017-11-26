8c00ed7a1cfeb9189467f9a632a50432575f5ae6
Silencious <tjh0473@gmail.com>
2017-11-25 17:31:11 -0800

    editor script & camera script

---
+++ Assets/Editor/SetShader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SetShaderGUI)), CanEditMultipleObjects]
+public class SetShader : Editor
+{
+	public override void OnInspectorGUI()
+	{
+
+		DrawDefaultInspector();
+
+		SetShaderGUI myScript = (SetShaderGUI)target;
+		if (GUILayout.Button("Select Meshes of Next GameObject"))
+		{
+			Selection.selectionChanged += myScript.SelectMeshes;
+		}
+		if (GUILayout.Button("Select All Meshes in Scene"))
+		{
+			myScript.SelectAll();
+		}
+		if (GUILayout.Button("Clear Meshes"))
+		{
+			myScript.ClearMeshes();
+		}
+		if (GUILayout.Button("Set Shader"))
+		{
+			myScript.SetShader();
+		}
+	}
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public float speed = 5.0f;
+	public float rotationSpeed = 100.0f;
+	void Update()
+	{
+		float tV, tH, rV, rH;
+		tV = tH = rV = rH = 0;
+		if (Input.GetKey(KeyCode.W))
+		{
+			tV += 1;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			tV -= 1;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			tH -= 1;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			tH += 1;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			rV -= 1;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			rV += 1;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			rH -= 1;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			rH += 1;
+		}
+		tV *= speed * Time.deltaTime;
+		tH *= speed * Time.deltaTime;
+		rV *= rotationSpeed * Time.deltaTime;
+		rH *= rotationSpeed * Time.deltaTime;
+		transform.Translate(tH, 0, tV);
+		transform.Rotate(rV, rH, 0);
+		var angle = transform.rotation.eulerAngles;
+		angle.z = 0;
+		transform.rotation = Quaternion.Euler(angle);
+	}
+}
