68a59a3dbca331d36785602422113a7505e14dd1
Silencious <tjh0473@gmail.com>
2017-11-27 19:11:03 -0800

    thickness

--- Assets/Scripts/SetShaderGUI.cs
+++ Assets/Scripts/SetShaderGUI.cs
@@ -3,31 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class SetShaderGUI : MonoBehaviour {
+public class SetShaderGUI : MonoBehaviour
+{
 	public Renderer[] meshes;
 	public Shader shader;
+	public float thickness = 0.000005f;
 
-	public void SelectMeshes(){
+	public void Update()
+	{
+		SetOutlineThick();
+	}
+
+	public void SelectMeshes()
+	{
 		var obj = Selection.activeGameObject;
 		Debug.Log(obj.name);
-		if(!obj) return;
+		if (!obj) return;
 		meshes = obj.GetComponentsInChildren<Renderer>();
 		Selection.selectionChanged -= SelectMeshes;
 	}
 
-	public void SelectAll(){
-		meshes = GameObject.FindObjectsOfType<Renderer>();	// only active loaded objs
+	public void SelectAll()
+	{
+		meshes = GameObject.FindObjectsOfType<Renderer>();  // only active loaded objs
 	}
 
-	public void ClearMeshes(){
+	public void ClearMeshes()
+	{
 		meshes = null;
 	}
 
-	public void SetShader(){
-		if(meshes == null) return;
-		foreach(var mesh in meshes){
-			foreach(var mat in mesh.sharedMaterials){
-				mat.shader = shader;
+	public void SetShader()
+	{
+		if (meshes == null) return;
+		foreach (var mesh in meshes)
+		{
+			if (mesh != null)
+			{
+				foreach (var mat in mesh.materials)
+				{
+					if (mat != null)
+					{
+						mat.shader = shader;
+					}
+				}
+			}
+		}
+	}
+
+	public void SetOutlineThick()
+	{
+		if (meshes == null) return;
+		Vector3 cam = Camera.main.transform.position;
+		foreach (var mesh in meshes)
+		{
+			if (mesh != null)
+			{
+				float d = (mesh.transform.position - cam).magnitude;
+				float thick = thickness * d * d;
+				foreach (var mat in mesh.materials)
+				{
+					if (mat != null)
+					{
+						mat.SetFloat("_Outline", thick);
+					}
+				}
 			}
 		}
 	}
