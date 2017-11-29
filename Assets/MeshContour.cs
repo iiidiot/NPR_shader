ac60ead3e52550cc39754bc3af4fcc64889ec228
yuniyuni <yunizhan@usc.edu>
2017-11-29 15:12:02 -0800

    mesh contour unrelated to camera view
    ugly ugly

--- Assets/MeshContour.cs
+++ Assets/MeshContour.cs
@@ -20,8 +20,8 @@ public class MeshContour : MonoBehaviour
 
 
     private Matrix4x4 model2CameraMatrix;
-    private Vector3 cameraFace = new Vector3(0, 0, -1);
-
+    private Vector3 cameraFace; // = new Vector3(0, 0, -1);
+    Vector4 E = new Vector4(0, 0, -1, 1);
 
     private bool doDraw;
 
@@ -36,7 +36,17 @@ public class MeshContour : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        
+        Vector4 temp = Camera.main.cameraToWorldMatrix * E;
+        cameraFace = new Vector3(temp.x, temp.y, temp.z);
+
+
+
         mesh = this.GetComponent<SkinnedMeshRenderer>().sharedMesh;
+
+        mesh.RecalculateNormals(); // ??
+
+
         vertices = mesh.vertices;
         normals = mesh.normals;
         triangles = mesh.triangles;
@@ -44,8 +54,96 @@ public class MeshContour : MonoBehaviour
 
         drawVertexList = new List<Vector3>();
 
+        drawVertexList.Clear();
+        int i, j;
+        List<int> tri1 = new List<int>();
+        bool doJudgeNorm;
+        int v1 = 0, v2 = 0;
+        int ev1 = 0, ev2 = 0;
+        for (i = 0; i < triangles.Length / 3; i++)
+        {
+            tri1.Add(triangles[i]);
+            tri1.Add(triangles[i + 1]);
+            tri1.Add(triangles[i + 2]);
+            for (j = 0; j < i; j++)
+            {
+                doJudgeNorm = false;
+                if (tri1.Remove(triangles[j])) // has j
+                {
+                    ev1 = triangles[j];
+                    if (tri1.Remove(triangles[j + 1])) // has j+1
+                    {
+                        // j~j+1 is common edge, judge (j+2)'s norm and tri1[0]'s norm
+                        doJudgeNorm = true;
+                        v1 = triangles[j + 2];
+                        v2 = tri1[0];
+
+                        ev2 = triangles[j + 1];
+                    }
+                    else if (tri1.Remove(triangles[j + 2]))
+                    {
+                        // j~j+2 is common edge, judge (j+1)'s norm and tri1[0]'s norm 
+                        doJudgeNorm = true;
+                        v1 = triangles[j + 1];
+                        v2 = tri1[0];
+
+                        ev2 = triangles[j + 2];
+                    }
+                    else
+                    {
+                        tri1.Add(triangles[j]);
+                    }
+                }
+                else if (tri1.Remove(triangles[j + 1])) // has j+1
+                {
+                    ev1 = triangles[j + 1];
+                    if (tri1.Remove(triangles[j + 2]))
+                    {
+                        // j+1~j+2 is common edge, judge (j)'s norm and tri1[0]'s norm 
+                        doJudgeNorm = true;
+                        v1 = triangles[j];
+                        v2 = tri1[0];
+                        ev2 = triangles[j + 2];
+                    }
+                    else
+                    {
+                        tri1.Add(triangles[j + 1]);
+                    }
+                }
+                if (doJudgeNorm)
+                {
+                    //if (Vector3.Dot(Model2Camera(normals[v1]), cameraFace) * Vector3.Dot(Model2Camera(normals[v2]), cameraFace) < 0)
+                    //if (Vector3.Dot(normals[v1], cameraFace) * Vector3.Dot(normals[v2], cameraFace) < 0)
+                    if (Vector3.Dot(normals[v1],normals[v2]) < 0)
+                    {
+                        //draw edge v1~v2
+                        drawVertexList.Add(vertices[ev1]);
+                        drawVertexList.Add(vertices[ev2]);
+                    }
+
+                }
+                j = j + 3;
+            }
+            i = i + 3;
+            tri1.Clear();
+        }
 
 
+        for (i = 0; i < drawVertexList.Count; i = i + 2)
+        {
+            GameObject newLine = Instantiate(Resources.Load("Line")) as GameObject;
+            newLine.transform.SetParent(this.transform);
+            newLine.transform.rotation = Quaternion.Euler(Vector3.zero);
+            newLine.transform.localEulerAngles = Vector3.zero;
+            newLine.transform.localScale = new Vector3(1, 1, 1);
+            LineRenderer lr = newLine.GetComponent<LineRenderer>();
+            lr.useWorldSpace = false;
+            lr.startWidth = 0.001f;
+            lr.endWidth = 0.001f;
+            lr.positionCount = 2;
+            lr.SetPosition(0, drawVertexList[i]);
+            lr.SetPosition(1, drawVertexList[i + 1]);
+        }
 
         // line renderer set-up
         //lr = this.GetComponent<LineRenderer>();
@@ -62,9 +160,9 @@ public class MeshContour : MonoBehaviour
         //   lr.positionCount++;
         //}
 
-        camPos = Camera.main.transform.position;
-        camRot = Camera.main.transform.rotation;
-        doDraw = true;
+        //camPos = Camera.main.transform.position;
+        //camRot = Camera.main.transform.rotation;
+        //doDraw = true;
     }
 
 
@@ -78,6 +176,7 @@ public class MeshContour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        return;
         int i; 
         if (camPos == Camera.main.transform.position && camRot == Camera.main.transform.rotation)
         {
@@ -106,6 +205,10 @@ public class MeshContour : MonoBehaviour
         
         camPos = Camera.main.transform.position;
         camRot = Camera.main.transform.rotation;
+
+
+        Vector4 temp = Camera.main.cameraToWorldMatrix * E;
+        cameraFace = new Vector3(temp.x, temp.y, temp.z);
 
         model2CameraMatrix = Camera.main.worldToCameraMatrix * transform.localToWorldMatrix;
 
@@ -167,7 +270,8 @@ public class MeshContour : MonoBehaviour
                 }
                 if (doJudgeNorm)
                 {
-                    if (Vector3.Dot(Model2Camera(normals[v1]), cameraFace) * Vector3.Dot(Model2Camera(normals[v2]), cameraFace) < 0)
+                    //if (Vector3.Dot(Model2Camera(normals[v1]), cameraFace) * Vector3.Dot(Model2Camera(normals[v2]), cameraFace) < 0)
+                    if (Vector3.Dot(normals[v1], cameraFace) * Vector3.Dot(normals[v2], cameraFace) < 0) 
                     {
                         //draw edge v1~v2
                         drawVertexList.Add(vertices[ev1]);
